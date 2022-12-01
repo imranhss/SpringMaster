4b79c9e71d07a25c4c816b2042c76923dadd2084
imranhss <emranhss@gmail.com>
2022-12-01 13:30:06 +0600

    Dao Complete

---
+++ TestSpringCrud/src/main/java/dao/StudentDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+import antlr.collections.List;
+import entity.Student;
+
+public class StudentDao {
+
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	@Transactional
+	public void create(Student s) {
+
+		hibernateTemplate.save(s);
+	}
+
+	public java.util.List<Student> getAll() {
+
+		java.util.List<Student> list = hibernateTemplate.loadAll(Student.class);
+
+		return list;
+
+	}
+
+	public void delete(int id) {
+
+		Student s = hibernateTemplate.load(Student.class, id);
+
+		hibernateTemplate.delete(s);
+
+	}
+
+	public Student getById(int id) {
+
+		return hibernateTemplate.get(Student.class, id);
+
+	}
+
+}
---
+++ TestSpringCrud/src/main/java/entity/Student.java
@@ -0,0 +1,91 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String gender;
+	private String cell;
+	private String subject;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String name, String email, String gender, String cell, String subject) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+		this.cell = cell;
+		this.subject = subject;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCell() {
+		return cell;
+	}
+
+	public void setCell(String cell) {
+		this.cell = cell;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", gender=" + gender + ", cell=" + cell
+				+ ", subject=" + subject + "]";
+	}
+	
+	
+
+}
