7ba427ce4f3e6358bab60e86aeaf752c28dbdf63
imranhss <emranhss@gmail.com>
2022-12-22 12:56:48 +0600

    TestBoot

---
+++ TestSpringBoot/src/main/java/org/idb/TestSpringBoot/entity/Employee.java
@@ -0,0 +1,33 @@
+package org.idb.TestSpringBoot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int eid;
+
+    @Column(length = 30, nullable = false)
+    private  String eName;
+    @Column(length = 45, nullable = false)
+    private  String eEmail;
+    @Column(length = 255, nullable = false)
+    private  String eAddress;
+    @Column(length = 12, nullable = false)
+    private  String eGender;
+    @Column(length = 25, nullable = false)
+    private  String eDepartment;
+    @ManyToOne
+    @JoinColumn(name = "did_fk")
+    Department department;
+
+}
